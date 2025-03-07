34f8592c8c4114a1b8ea49cfa24b4b39609c41d1
roshanshresthanp <stha901@gmail.com>
2025-03-07 16:16:25 +0000

    Deleted all ophotos

--- src/main/java/com/example/hotelapp/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hotelapp.repository;
-
-import com.example.hotelapp.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Admin findByUsername(String username);
-}
--- src/main/java/com/example/hotelapp/repository/HotelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hotelapp.repository;
-
-import com.example.hotelapp.entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-    Hotel findByUsername(String username);
-}
